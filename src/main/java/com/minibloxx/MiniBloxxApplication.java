6c87a3551cdf607b1bb05f9b0e0644f6fa13c709
Saad Fihi <fihisaad4@gmail.com>
2026-07-01 20:36:32 +0200

    Project Initialisation

---
+++ src/main/java/com/minibloxx/MiniBloxxApplication.java
@@ -0,0 +1,13 @@
+package com.minibloxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBloxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBloxxApplication.class, args);
+	}
+
+}
